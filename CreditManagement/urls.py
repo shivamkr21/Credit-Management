98874b937390bbdb790491eab13d470eaa965f0b
ShauryaSingh2009 <72138383+ShauryaSingh2009@users.noreply.github.com>
2021-10-01 09:02:40 +0530

    Update urls.py

--- CreditManagement/urls.py
+++ CreditManagement/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import path
+from . import views
 
 urlpatterns = [
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+	url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     url(r'^admin/', admin.site.urls),
     url(r'',include('Manage.urls')),
 ]
